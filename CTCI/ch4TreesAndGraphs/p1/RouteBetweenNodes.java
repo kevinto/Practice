c025c52656ca3cc6c3328360631ed21d52055ae9
Kevin To <kevinto1989@gmail.com>
2016-03-22 05:29:18 +0000

    added base code for route between nodes problem

---
+++ CTCI/ch4TreesAndGraphs/p1/RouteBetweenNodes.java
@@ -0,0 +1,11 @@
+public class RouteBetweenNodes {
+    public static void main (String args[]) {
+        System.out.println("hello world");
+    }
+    
+    // Route Between Nodes: Given a directed graph, design an algorithm 
+    // to find out whether there is a route between two nodes.
+    public static void routeExists(String grid) {
+        return;
+    } 
+}
