3925ec935aececd83c5dd3e7ac7cf581972996c4
Kevin To <kevinto1989@gmail.com>
2016-03-23 01:58:48 +0000

    added pseudocode for route between nodes problem

--- CTCI/ch4TreesAndGraphs/p1/RouteBetweenNodes.java
+++ CTCI/ch4TreesAndGraphs/p1/RouteBetweenNodes.java
@@ -1,11 +1,27 @@
 public class RouteBetweenNodes {
     public static void main (String args[]) {
-        System.out.println("hello world");
+        
     }
     
     // Route Between Nodes: Given a directed graph, design an algorithm 
     // to find out whether there is a route between two nodes.
-    public static void routeExists(String grid) {
+    // Assume I am given the Adjacency list.
+    // Assume I am given the start point and the end point.
+    public static void routeExists(String grid, String start, String end) {
+        // Data structure: Adjacency list implemented with a linked list
+        
+        // Add start to an empty queue
+        
+        // While the queue is not empty:
+            // Set the current element to a dequeued
+            
+            // for each node n that is adjacent to current:
+                // if node is what we are looking for
+                    // return true
+                // if node is unvisited
+                    // add n to the queue
+                
+        
         return;
     } 
 }
