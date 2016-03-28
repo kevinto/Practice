b313d1654917d2370be021c1493f0674e91b6608
Kevin To <kevinto1989@gmail.com>
2016-03-28 16:30:54 +0000

    added additional leetcode problem

---
+++ LeetCode/171/titleToNumber.java
@@ -0,0 +1,21 @@
+// Problem: Given a column title as appear in an Excel sheet, 
+//          return its corresponding column number.
+
+// Examples: (input -> output)
+// A -> 1
+// B -> 2
+// C -> 3
+// ...
+// Z -> 26
+// AA -> 27
+// AB -> 28 
+
+// Questions:
+// 1. What is the pattern for the column names?
+// 2. Is there a max column number?
+
+public class titleToNumber {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
