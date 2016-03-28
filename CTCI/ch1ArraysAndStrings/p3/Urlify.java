223dfaa0f4ed179bef7adc2f3b54bc09f1429c88
Kevin To <kevinto1989@gmail.com>
2016-03-28 06:49:52 +0000

    added my own implementation of urlify

--- CTCI/ch1ArraysAndStrings/p3/Urlify.java
+++ CTCI/ch1ArraysAndStrings/p3/Urlify.java
@@ -1,35 +1,49 @@
 public class Urlify {
-    
+  // Assumptions:
+    // 1. There is enough space in the original string to do in string replacement.
+    // 2. There can be multiple spaces between words.
+  
+  public static void main(String[] args) {
+    String testString = "Mr John Smith           ";
+    char[] testCharArr = testString.toCharArray();
+    System.out.println(urlify(testCharArr, 13));
+  }
+  
   public static char[] urlify(char[] str, int length) {
     int numSpaces = countSpaces(str, length);
-    int endPtr = length - 1 + numSpaces * 2;
+    
+    // Set a point where we can start writing characters to the final string.
+    // This newLength calcuation makes each space take 3 characters.
+    int newLength = length + (numSpaces * 2);
+    str[newLength] = '\0'; // Adds a terminator to the end of the new string
+   
+    // Start iteratoring backwards from the length of the original string.
+    // Populate backwards starting from newLength. We are essentially
+    // tracking two parts of the string.
     for (int i = length - 1; i >= 0; i--) {
       if (str[i] == ' ') {
-        str[endPtr] = '0';
-        str[endPtr - 1] = '2';
-        str[endPtr - 2] = '%';
-        endPtr -= 3;
-      } else {
-        str[endPtr] = str[i];
-        endPtr--;
+        str[newLength - 1] = '0';
+        str[newLength - 2] = '2';
+        str[newLength - 3] = '%';
+        newLength -= 3;
+      }
+      else {
+        str[newLength - 1] = str[i];
+        newLength -= 1;
       }
     }
+    
     return str;
   }
   
-  private static int countSpaces(char[] str, int length) {
-    int numSpaces = 0;
+  public static int countSpaces(char[] str, int length) {
+    int count = 0;
     for (int i = 0; i < length; i++) {
       if (str[i] == ' ') {
-        numSpaces++;
+        count++;
       }
     }
-    return numSpaces;
-  }
-  
-  public static void main(String[] args) {
-    String testString = "Mr John Smith";
-    char[] testCharArr = testString.toCharArray();
-    System.out.println(urlify(testCharArr, 13));
+    
+    return count;
   }
 }
