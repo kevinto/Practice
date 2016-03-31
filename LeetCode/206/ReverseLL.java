ed0a42516e66aa0ce60a831e5dced74b8d244ddf
Kevin To <kevinto1989@gmail.com>
2016-03-31 06:24:38 +0000

    added new problem

---
+++ LeetCode/206/ReverseLL.java
@@ -0,0 +1,17 @@
+/*
+
+Link: https://leetcode.com/problems/reverse-linked-list/
+Problem: Reverse a singly linked list.
+
+1. Do we want to reverse in place?
+2. 
+*/
+import java.util.LinkedList;
+public class ReverseLL {
+    public static void main(String[] args) {
+        LinkedList<String> ll = new LinkedList<String>();
+        ll.add("A");
+        ll.add("B");
+        ll.add("C");
+    }
+}
