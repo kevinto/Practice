7a6d14d5dd017259c403246302461c6f69d5d3e3
kevinto@github.com <kevinto@github.com>
2016-04-04 09:34:13 -0700

    added lca of binary tree leetcode problem

---
+++ leetCode/p235/LcaOfBinaryTree.java
@@ -0,0 +1,25 @@
+package p235;
+
+/**
+ * Created by Kevin on 4/4/16.
+ * Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ *
+ * Problem: Given a binary search tree (BST), find the lowest common ancestor (LCA)
+ *          of two given nodes in the BST.
+ *
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor is
+ * defined between two nodes v and w as the lowest node in T that has both v and w
+ * as descendants (where we allow a node to be a descendant of itself).”
+ *
+ * 1. Do i need to optimize for space or time?
+ *
+ * Initial solution:
+ *
+ *
+ */
+
+public class LcaOfBinaryTree {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
