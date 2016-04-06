fd9733cd2cf8da3eb0c3f00710484df00df794b2
kevinto <to.thanh.kevin@gmail.com>
2016-04-05 19:05:50 -0700

    Linked list added the delete method

--- DataStructures/LinkedList.java
+++ DataStructures/LinkedList.java
@@ -11,10 +11,20 @@ public class LinkedList {
         // Sample linked list: 1 -> 2 -> 3 -> 4 -> 5
         // Create methods to modify a single node
 
+        System.out.println("New list created: ");
         LinkedListDs ll = new LinkedListDs();
         ll.add(1);
         ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        ll.add(5);
         ll.printList();
+        System.out.println("size: " + ll.size);
+
+        System.out.println("Deleting head: ");
+        ll.delete(1);
+        ll.printList();
+        System.out.println("size: " + ll.size);
     }
 }
 
@@ -36,12 +46,24 @@ class LinkedListDs {
         }
     }
 
+    public void delete(int value) {
+        node temp = head;
+        while(temp != null) {
+            if (temp.value == value) {
+                size--;
+                head = temp.next;
+            }
+            temp = temp.next;
+        }
+    }
+
     public void printList() {
         node tempPtr = head;
         while (tempPtr != null) {
             System.out.print(tempPtr.value + " ");
             tempPtr = tempPtr.next;
         }
+        System.out.println("");
     }
 }
 
