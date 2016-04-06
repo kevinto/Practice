6c6faf9bc71d3a19cd87058746e4cc5790c03122
kevinto@github.com <kevinto@github.com>
2016-04-06 09:05:41 -0700

    Added a skeleton stack class

---
+++ DataStructures/StackClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Kevin on 4/6/16.
+ *
+ * Stack is a FIFO data structure.
+ * Main methods:
+ *      push() - adds a new item to the top of the stack.
+ *      pop() - removes the top item and returns it to the caller
+ */
+public class StackClass {
+}
