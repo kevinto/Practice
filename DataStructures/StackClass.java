53c90431410ffdbb5ade2d31b8af6351c4bd8fab
kevinto@github.com <kevinto@github.com>
2016-04-07 08:47:30 -0700

    finished implementation of the stack class

--- DataStructures/StackClass.java
+++ DataStructures/StackClass.java
@@ -1,3 +1,5 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  * Created by Kevin on 4/6/16.
  *
@@ -7,10 +9,52 @@
  *      pop() - removes the top item and returns it to the caller
  */
 public class StackClass {
+    public static void main(String[] args) {
+        Stackx s = new Stackx();
+        s.push(1);
+        s.push(2);
+        Node poppedNode = s.pop();
+        s.printStack();
+        System.out.println("popped node: " + poppedNode.value);
+    }
 }
 
-class Stack {
+class Stackx {
+    public int size = 0;
+    private Node head;
 
+    public void push(int value) {
+        if (head == null) {
+            head = new Node(value);
+            size++;
+        }
+        else {
+            Node newHead = new Node(value);
+            newHead.next = head;
+            head = newHead;
+            size++;
+        }
+    }
+
+    public Node pop() {
+        if (head == null) {
+            return null;
+        }
+
+        Node retNode = head;
+        head = head.next;
+
+        return retNode;
+    }
+
+    // For debugging
+    public void printStack() {
+        Node currPtr = head;
+        while (currPtr != null) {
+            System.out.println(currPtr.value);
+            currPtr = currPtr.next;
+        }
+    }
 }
 
 class Node {
