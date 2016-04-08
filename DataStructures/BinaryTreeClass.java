afdcb906c11fc4555f4453f648cbb84510faa8df
kevinto <to.thanh.kevin@gmail.com>
2016-04-07 17:08:01 -0700

    added skeleton for binary tree

---
+++ DataStructures/BinaryTreeClass.java
@@ -0,0 +1,15 @@
+/**
+ * Created by kevint on 4/7/2016.
+ */
+public class BinaryTreeClass {
+    public static void main(String[] args) {
+
+    }
+}
+
+class BinaryTreeNode {
+    public int value;
+    BinaryTreeNode left;
+    BinaryTreeNode right;
+    BinaryTreeNode(int x) { value = x; }
+}
