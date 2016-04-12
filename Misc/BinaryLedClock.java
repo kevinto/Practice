944837c2ce901cf5500298222eb151f31c3f340f
kevinto <to.thanh.kevin@gmail.com>
2016-04-11 18:19:09 -0700

    added new misc problem

---
+++ Misc/BinaryLedClock.java
@@ -0,0 +1,12 @@
+/**
+ * Created by kevint on 4/11/2016.
+ *
+ * Problem: The twelve hour clock format is:
+ * 0000 : 000000 in binary. Do not worry about AM/PM.
+ * Find all the times that have three ones in it only.
+ */
+public class BinaryLedClock {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
