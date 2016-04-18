82f17181cd83884b74581fbdfb18465cdd6a1857
kevinto <to.thanh.kevin@gmail.com>
2016-04-18 09:38:51 -0700

    Added new leetcode problem

---
+++ leetCode/p202/HappyNumber.java
@@ -0,0 +1,18 @@
+package p202;
+
+/**
+ * Created by kevint on 4/18/2016.
+ */
+public class HappyNumber {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public static boolean isHappy(int n) {
+        // convert number to string
+        // check hash table to see if it exists
+        // for each digit in the string convert to int and square and sum
+        // convert to string and repeat
+        return false;
+    }
+}
