0a4ec0b095e85aae4b56928cce78ace28d72a136
kevinto <to.thanh.kevin@gmail.com>
2016-04-21 19:02:42 -0700

    Added oop tutorial files

--- JavaPractice/BitShifting.java
+++ JavaPractice/BitShifting.java
@@ -1,10 +1,15 @@
 /**
  * Created by Kevin on 4/11/16.
  */
+import SamplePackage.Person;
 public class BitShifting {
     public static void main(String[] args) {
         int bitMask = 0x000F;
         int val = 0x2222; //what does this mean? This is in hex
         System.out.println(val & bitMask);
+
+        Person p = new Person();
+        p.setName("kevin");
+        System.out.println(p.getName());
     }
 }
---
+++ JavaPractice/SamplePackage/Person.java
@@ -0,0 +1,19 @@
+package SamplePackage;
+/**
+ * Created by kevint on 4/21/2016.
+ */
+public class Person {
+    private String name;
+    private int age;
+    private int weight;
+    private String eyeColor;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
