faa30347bf37712bdee28ee7556c49fe88f89737
kevinto <to.thanh.kevin@gmail.com>
2016-04-29 09:23:41 -0700

    Added first implementation for three fixed stack

--- ctci/ch2StacksAndQueues/ThreeInOne.java
+++ ctci/ch2StacksAndQueues/ThreeInOne.java
@@ -1,5 +1,7 @@
 package ch2StacksAndQueues;
 
+import java.util.EmptyStackException;
+
 /**
  * Created by Kevin on 4/28/16.
  *
@@ -21,22 +23,72 @@ package ch2StacksAndQueues;
  *
  * 3. Does the user control which stack to push and pop from?
  * 4. Will there be a max stack size?
+ * 5. Does out solution have to take care of a stack running out of space?
+ * 6. Can we assume that stack 1 is identified as 0, stack 2 as 1, stack 3 as 2?
  */
 public class ThreeInOne {
     public static void main(String args[]) {
+        try {
+            FixedMultiStack fs = new FixedMultiStack(3);
+            fs.push(0, 1);
+        }
+        catch(Exception ex) {
+        }
 
     }
 }
 
-class ThreeStack {
-    private int allocatedPerStackSize = 4;
-    private int stack1InsertIdx;
-    private int stack2InsertIdx;
-    private int stack3InsertIdx;
-    private int stack1EndIdx;
-    private int stack2EndIdx;
-    private int stack3EndIdx;
-    private int stack1Size;
-    private int stack2Size;
-    private int stack3Size;
+class FixedMultiStack {
+    private int numberOfStacks = 3;
+    private int stackCapacity;
+    private int[] values;
+    private int[] sizes;
+
+    public FixedMultiStack(int stackSize) {
+        stackCapacity = stackSize;
+        values = new int[stackSize * numberOfStacks];
+        sizes = new int[numberOfStacks];
+    }
+
+    public void push(int stackNum, int value) throws FullStackException {
+        if (isFull(stackNum)) {
+            throw new FullStackException();
+        }
+
+        sizes[stackNum]++;
+        values[indexOfTop(stackNum)] = value;
+    }
+
+    public int pop(int stackNum) {
+        if (isEmpty(stackNum)) {
+            throw new EmptyStackException();
+        }
+
+        sizes[stackNum]--;
+        return values[indexOfTop(stackNum)];
+    }
+
+    public int peek(int stackNum) {
+        if (isEmpty(stackNum)) {
+            throw new EmptyStackException();
+        }
+        return values[indexOfTop(stackNum)];
+    }
+
+    private boolean isFull(int stackNum) {
+        return sizes[stackNum] == stackCapacity;
+    }
+
+    private int indexOfTop(int stackNum) {
+        int offset = stackNum * stackCapacity;
+        int size = sizes[stackNum];
+        return offset + size - 1;
+    }
+
+    private boolean isEmpty(int stackNum) {
+        return sizes[stackNum] == 0;
+    }
+}
+
+class FullStackException extends Exception {
 }
