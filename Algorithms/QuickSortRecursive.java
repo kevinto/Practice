12712ec92b3e1d683410f7ecd55a5a1343ee3677
kevinto <to.thanh.kevin@gmail.com>
2016-05-04 11:20:44 -0700

    Added notes for quicksort

--- Algorithms/QuickSortRecursive.java
+++ Algorithms/QuickSortRecursive.java
@@ -2,19 +2,38 @@ import java.util.Arrays;
 
 /**
  * Created by kevint on 4/27/2016.
+ *
+ * Alternatives to picking the pivot:
+ *  1. Pick the first element of the array.
+ *  2. Pick the last element of the array.
+ *  3. Pick a random element as pivot.
+ *  4. Pick median as pivot.
+ *
+ *
+ * Runtime analysis: O(n log(n))
+ * Memory analysis: If it was just the space taken up in the original array
+ *                  then the space complexity would be O(1)??. Since we are
+ *                  using recursion and we recurse (log n) times, then the
+ *                  memory on the stack would grow on the order of O(log(n))
+ *                  in the average case. This assumes that we have a reasonable
+ *                  pivot in the average case. In the worse case, the space
+ *                  complexity is O(n). This is because we will do n recursive
+ *                  calls? todo: why?
+ *                  WHEN TALKING ABOUT THIS BE SURE TO MENTION EXTRA SPACE.
+ *                  WE DON'T CARE ABOUT THE SPACE THE ARRAY TAKES UP.
  */
 public class QuickSortRecursive {
     public static void main(String[] args) {
         int[] test1 = {10, 7, 8, 9, 1, 5};
 
         System.out.println("Before: " + Arrays.toString(test1));
-        QSort qSort = new QSort();
+        QSortRecursive qSort = new QSortRecursive();
         qSort.sort(test1, 0, test1.length - 1);
         System.out.println("After: " + Arrays.toString(test1));
     }
 }
 
-class QSort {
+class QSortRecursive {
     void sort(int arr[], int low, int high) {
         if (low < high) {
             int partitioningIdx = partition(arr, low, high);
