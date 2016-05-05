ca42f1756f4ef76f2e8382279ff5ed356cfc982c
kevinto <to.thanh.kevin@gmail.com>
2016-05-04 18:57:44 -0700

    Quicksort recursive added more comments

--- Algorithms/QuickSortRecursive.java
+++ Algorithms/QuickSortRecursive.java
@@ -26,48 +26,65 @@ import java.util.Arrays;
  */
 public class QuickSortRecursive {
     public static void main(String[] args) {
-        int[] test1 = {10, 7, 8, 9, 1, 5};
+        int[] test1 = {3, 7, 8, 9, 1, 5};
 
         System.out.println("Before: " + Arrays.toString(test1));
         QSortRecursive qSort = new QSortRecursive();
         qSort.sort(test1, 0, test1.length - 1);
         System.out.println("After: " + Arrays.toString(test1));
+
+        System.out.println("");
+
+        int[] test2 = {10, 7, 8, 99, 1, 5, 100, 200, 40, 89, 60};
+        System.out.println("Before: " + Arrays.toString(test2));
+        qSort.sort(test2, 0, test2.length - 1);
+        System.out.println("After: " + Arrays.toString(test2));
     }
 }
 
 class QSortRecursive {
     void sort(int arr[], int low, int high) {
         if (low < high) {
+            // Find where we can split the two arrays
             int partitioningIdx = partition(arr, low, high);
 
+            // Process the left side of the array
             sort(arr, low, partitioningIdx - 1);
+
+            // Process the right side of the array
             sort(arr, partitioningIdx + 1, high);
         }
     }
 
     int partition(int arr[], int low, int high) {
-        int pivot = arr[high];
-        int i = (low - 1); // Tracks all the elements that
-                           // are less than the pivot.
+        int pivot = arr[high]; // Pivot to the rightmost element
+
+        // i represents an element Idx that can be swapped
+        // with a value that is greater than the pivot
+        int i = low;
 
         for (int j = low; j <= high - 1; j++) {
             if (arr[j] <= pivot) {
-                i++;
-
                 swap(arr, i, j);
+                i++;
             }
         }
 
         // i tracks the last element that was smaller than
         // the pivot. Here, we swap the pivot with the number
         // directly after the last number that was smaller
-        // than the pivot.
-        swap(arr, i + 1, high);
+        // than the pivot. If the pivot is already in the
+        // correct place, then i is already equal to high.
+        swap(arr, i, high);
 
-        return i + 1;
+        return i;
     }
 
     void swap(int[] arr, int swapIdx1, int swapIdx2) {
+        if (swapIdx1 == swapIdx2) {
+            return;
+        }
+
         int temp = arr[swapIdx1];
         arr[swapIdx1] = arr[swapIdx2];
         arr[swapIdx2] = temp;
