e34e8ae155150622f29fbf1a939750de1475cf6f
kevinto <to.thanh.kevin@gmail.com>
2016-05-08 13:29:45 -0700

    Added solution for top k problem

--- leetCode/p347CURRENT/TopK.java
+++ leetCode/p347CURRENT/TopK.java
@@ -1,47 +1,68 @@
 package p347CURRENT;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Kevin on 5/6/16.
  * Problem Link: https://leetcode.com/problems/top-k-frequent-elements/
  *
  * 1. Use a hashmap with key equal to the number and value equal
- *    to the number of occurences. Finding the num occurences
- *    would be O(n). Then you have to sort the num occurences
+ *    to the number of occurrences. Finding the num occurrences
+ *    would be O(n). Then you have to sort the num occurrences
  *    to find the k most frequent elements.
  * 2. Create a temp array at least the size of the input array.
- *    Initialize to zero and increment the num occurances.
+ *    Initialize to zero and increment the num occurrences.
  *    Sort the temp array using quick sort.
  *    Time complexity: O(n) + (n log n)
+ * 3. Use bucket sort.
+ *    Time complexity: O(n)
  *
- * Q1: Will I ever get an empty array? Based on the assmption, no.
+ * Q1: Will I ever get an empty array? Based on the assumption, no.
  *
  */
 public class TopK {
     public static void main(String[] args) {
-        int[] arr1 = {1, 1, 1, 2, 2, 3};
+        int[] arr1 = {1, 1, 1, 2, 2, 2, 2, 3};
         System.out.println(topKFrequent(arr1, 2).toString());
+
+        int[] arr2 = {1};
+        System.out.println(topKFrequent(arr2, 1).toString());
     }
 
     public static List<Integer> topKFrequent(int[] nums, int k) {
-        int[] occurs = new int[nums.length];
+        List<Integer>[] bucket = new List[nums.length + 1];
+        HashMap<Integer, Integer> freqMap = new HashMap<Integer, Integer>();
 
-        for(int i = 0; i < nums.length; i++) {
-            occurs[nums[i]]++;
+        // Create a map with keys as the number and values as the number of
+        // occurrences
+        for (int n : nums) {
+            freqMap.put(n, freqMap.getOrDefault(n, 0) + 1);
         }
 
-        Arrays.sort(occurs);
-
-        List<Integer> returnList = new ArrayList<>();
-        for(int i = nums.length - 1; i > nums.length - k - 1; i--) {
-            //TODO bug here we need to return the numbers
-            returnList.add(occurs[i]);
+        // Add keys from the hashmap to an array using the number of occurrences
+        // as the index
+        for (int key : freqMap.keySet()) {
+            int freq = freqMap.get(key);
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
         }
 
-        return returnList;
+
+        // Start from the right side of the bucket array and add the key
+        // values to the result array
+        List<Integer> res = new ArrayList<>();
+        for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+            if (bucket[pos] != null) {
+                res.addAll(bucket[pos]);
+            }
+        }
+
+        return res;
     }
 }
+
