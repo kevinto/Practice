a23caca9e7d38b5be67b4568213a75a66343af41
kevinto <to.thanh.kevin@gmail.com>
2016-05-14 09:58:45 -0700

    Added new leetcode problem

---
+++ leetCode/p334CURR/IncreasingTripletSubsequence.java
@@ -0,0 +1,20 @@
+package p334CURR;
+
+/**
+ * Created by Kevin on 5/14/16.
+ * Link: https://leetcode.com/problems/increasing-triplet-subsequence/
+ *
+ * Problem: Given an unsorted array return whether an increasing
+ *          subsequence of length 3 exists or not in the array.
+ *
+ *          Return true if there exists i, j, k such that
+ *          arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false.
+ *
+ *          Examples:
+ *          1. [1, 2, 3, 4, 5] = true
+ *          2. [5, 2, 3, 4, 1] = true
+ *          3. [1,1,1,5] = false
+ *          4. [5, 4, 3, 2, 1] = false
+ */
+public class IncreasingTripletSubsequence {
+}
