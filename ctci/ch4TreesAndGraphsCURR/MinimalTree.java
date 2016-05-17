975efbf0225ad8a6d302825383dcc3ad711e75e8
kevinto <to.thanh.kevin@gmail.com>
2016-05-16 19:21:52 -0500

    Added solution for minimal tree problem

--- ctci/ch4TreesAndGraphsCURR/MinimalTree.java
+++ ctci/ch4TreesAndGraphsCURR/MinimalTree.java
@@ -9,4 +9,45 @@ package ch4TreesAndGraphsCURR;
  *  What is the difference between height and depth?
  */
 public class MinimalTree {
+    public static void main(String[] args) {
+        int[] testArr1 = { 1, 2, 3, 4, 5, 6 };
+        NodeBST root = minBstTree(0, testArr1.length - 1, testArr1);
+        printTree(root);
+
+        return;
+    }
+
+    public static NodeBST minBstTree(int start, int end, int[] arr) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+        NodeBST root = new NodeBST(arr[mid]);
+
+        root.left = minBstTree(start, mid - 1, arr);
+        root.right = minBstTree(mid + 1, end, arr);
+
+        return root;
+    }
+
+    public static void printTree(NodeBST root) {
+        if (root == null) {
+            return;
+        }
+
+        printTree(root.left);
+        System.out.println(root.val);
+        printTree(root.right);
+    }
+}
+
+class NodeBST {
+    int val;
+    NodeBST left;
+    NodeBST right;
+
+    NodeBST(int x) {
+        this.val = x;
+    }
 }
