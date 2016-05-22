968f2ddb3d3ec6be6ebc6be7a5aa7b284785f4b8
kevinto <to.thanh.kevin@gmail.com>
2016-05-21 21:21:44 -0400

    Added data structures for build order problem

--- ctci/ch4TreesAndGraphsCURR/BuildOrder.java
+++ ctci/ch4TreesAndGraphsCURR/BuildOrder.java
@@ -1,8 +1,72 @@
 package ch4TreesAndGraphsCURR;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by Kevin on 5/21/16.
  */
 public class BuildOrder {
 
+}
+
+class Graph {
+    private ArrayList<Project> nodes = new ArrayList<>();
+    private HashMap<String, Project> map = new HashMap<>();
+
+    public Project getOrCreateNode(String name) {
+        if (!map.containsKey(name)) {
+            Project node = new Project(name);
+            nodes.add(node);
+            map.put(name, node);
+        }
+
+        return map.get(name);
+    }
+
+    public void addEdge(String startName, String endName) {
+        Project start = getOrCreateNode(startName);
+        Project end = getOrCreateNode(endName);
+        start.addNeighbor(end);
+    }
+
+    public ArrayList<Project> getNodes() {
+        return nodes;
+    }
+}
+
+class Project {
+    private ArrayList<Project> children = new ArrayList<>();
+    private HashMap<String, Project> map = new HashMap<>();
+    private String name;
+    private int dependencies = 0;
+
+    public Project(String n) { name = n; }
+
+    public void addNeighbor(Project node) {
+        if (!map.containsKey(node.getName())) {
+            children.add(node);
+            node.incrementDependencies();
+        }
+    }
+
+    public void incrementDependencies() {
+        dependencies++;
+    }
+
+    public void decrementDependencies() {
+        dependencies--;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Project> getChildren() {
+        return children;
+    }
+
+    public int getDependencies() {
+        return dependencies;
+    }
 }
