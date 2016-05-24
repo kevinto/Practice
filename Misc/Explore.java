fb6110a65ec4540d42336c5a365534cf89295ada
Kevin To <to.thanh.kevin@gmail.com>
2016-05-24 00:06:10 -0700

    Added experiemental greedy solution implementation

--- Misc/Explore.java
+++ Misc/Explore.java
@@ -5,9 +5,26 @@ import java.util.Stack;
  */
 public class Explore {
     public static void main(String[] args) {
-        Stack<Integer> s = new Stack<>();
-        int val = s.pop();
-        System.out.println(val);
+//        Stack<Integer> s = new Stack<>();
+//        int val = s.pop();
+//        System.out.println(val);
+        int[] arr = {1, 2, 4, 13, 43};
+        System.out.println(minPatches(arr, 100));
+    }
+
+    public static int minPatches(int[] nums, int n) {
+        long miss = 1, added = 0;
+        int i = 0;
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss += nums[i++];
+            }
+            else {
+                miss += miss;
+                added++;
+            }
+        }
+        return (int)added;
     }
 
     public static String reverseString(String s) {
