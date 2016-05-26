560af0c456f42837220d27046d94065ae70df31b
Kevin To <to.thanh.kevin@gmail.com>
2016-05-25 22:42:28 -0700

    Added arraylist

--- JavaPractice/DS/DS.java
+++ JavaPractice/DS/DS.java
@@ -1,6 +1,9 @@
 package DS;
 
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by kevin on 5/23/2016.
@@ -8,6 +11,7 @@ import java.util.BitSet;
 public class DS {
     public static void main(String[] args) {
         runBitSet();
+        runArrayList();
     }
 
     public static void runBitSet() {
@@ -22,5 +26,26 @@ public class DS {
         System.out.println("Initial pattern in bits1: ");
         System.out.println(bits1); // Only prints the bits that are set
         System.out.println(bits1.get(1));
+    }
+
+    public static void runArrayList() {
+        List<String> arrList = new ArrayList<>();
+        System.out.println("Initial size of arraylist: " + arrList.size());
+
+        arrList.add("C");
+        arrList.add("A");
+        arrList.add("E");
+        System.out.println("Size after additions: " + arrList.size());
+
+        arrList.remove(0);
+        System.out.println("Size after deletions: " + arrList.size());
+        System.out.println("Contents: " + arrList);
+
+        System.out.println("Print arraylist contents using iterator: ");
+        Iterator itr = arrList.listIterator();
+        while (itr.hasNext()) {
+            Object element = itr.next();
+            System.out.print(element + " ");
+        }
     }
 }
