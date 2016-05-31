2b1d5b8f94cedaafc1f3b440f0eb43fa91562dfd
kevinto <to.thanh.kevin@gmail.com>
2016-05-31 16:18:31 -0700

    Explore java's comparator and comparable methods

---
+++ JavaPractice/ComparatorPractice.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+/**
+ * Created by kevint on 5/31/2016.
+ */
+public class ComparatorPractice {
+    public static void main(String[] args) {
+        List<Dog> list = new ArrayList<Dog>();
+        list.add(new Dog("Shaggy",3));
+        list.add(new Dog("Lacy", 2));
+        list.add(new Dog("Roger", 10));
+        list.add(new Dog("Tommy", 4));
+        list.add(new Dog("Tammy", 1));
+
+        // Sorts by natural ordering. Uses the defined compareTo()
+        // for the comparable interface
+        Collections.sort(list);
+        for (Dog d : list) {
+            System.out.print(d.getDogName() + ", ");
+        }
+        System.out.println();
+
+        // Sorts by age. Defined in the compare() for the
+        // comparable interface
+        Collections.sort(list, new Dog());
+        for (Dog d : list) {
+            System.out.print(d.getDogName() + ": " + d.getDogAge() + ", ");
+        }
+    }
+}
+
+class Dog implements Comparator<Dog>, Comparable<Dog> {
+    private String name;
+    private int age;
+
+    Dog() {}
+
+    Dog(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    public String getDogName() {
+        return name;
+    }
+
+    public int getDogAge() {
+        return age;
+    }
+
+    public int compareTo(Dog d) {
+        return (this.name).compareTo(d.name);
+    }
+
+    // Overrride for the comparator
+    public int compare(Dog d1, Dog d2) {
+        return d1.age - d2.age;
+    }
+}
+
+
--- Misc/Explore.java
+++ Misc/Explore.java
@@ -1,17 +1,55 @@
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Created by kevint on 4/27/2016.
  */
 public class Explore {
     public static void main(String[] args) {
-//        Stack<Integer> s = new Stack<>();
-//        int val = s.pop();
-//        System.out.println(val);
-        int[] arr = {1, 2, 4, 13, 43};
-        System.out.println(minPatches(arr, 100));
     }
+    public int[] intersect(int[] nums1, int[] nums2) {
+        // Determine return array size
+        List<Integer> arrList = new ArrayList<>();
 
+        HashMap<Integer,Integer> hm1 = new HashMap<>();
+        for (int num : nums1) {
+            if (hm1.containsKey(num)) {
+                int currVal1 = hm1.get(num);
+                hm1.put(num, currVal1++);
+            }
+            else {
+                hm1.put(num, 1);
+            }
+        }
+
+        HashMap<Integer,Integer> hm2 = new HashMap<>();
+        for (int num : nums2) {
+            if (hm2.containsKey(num)) {
+                int currVal2 = hm2.get(num);
+                hm2.put(num, currVal2++);
+            }
+            else {
+                hm2.put(num, 1);
+            }
+        }
+
+        for (int key : hm1.keySet()) {
+            if (hm2.containsKey(key)) {
+                int v1 = hm1.get(key);
+                int v2 = hm2.get(key);
+                int max = Math.max(v1, v2);
+
+                for (int i = 0; i < max; i++) {
+                    arrList.add(key);
+                }
+            }
+        }
+
+        int[] returnArr = new int[arrList.size()];
+        for (int i = 0; i < arrList.size(); i++) {
+            returnArr[i] = arrList.get(i);
+        }
+        return returnArr;
+    }
     public static int minPatches(int[] nums, int n) {
         long miss = 1, added = 0;
         int i = 0;
