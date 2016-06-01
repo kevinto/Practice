deb6242c82c8a889abc3c260778532edcdd43039
kevinto <to.thanh.kevin@gmail.com>
2016-06-01 09:37:51 -0700

    Added new explore problem

--- Misc/Explore.java
+++ Misc/Explore.java
@@ -1,68 +1,44 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
 
 /**
  * Created by kevint on 4/27/2016.
  */
 public class Explore {
     public static void main(String[] args) {
+        ArrayList<Integer> test = getRow(2);
+        System.out.println((test));
     }
-    public int[] intersect(int[] nums1, int[] nums2) {
-        // Determine return array size
-        List<Integer> arrList = new ArrayList<>();
 
-        HashMap<Integer,Integer> hm1 = new HashMap<>();
-        for (int num : nums1) {
-            if (hm1.containsKey(num)) {
-                int currVal1 = hm1.get(num);
-                hm1.put(num, currVal1++);
-            }
-            else {
-                hm1.put(num, 1);
-            }
-        }
+    public static ArrayList<Integer> getRow(int a) {
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(1); // Adds the header 1
 
-        HashMap<Integer,Integer> hm2 = new HashMap<>();
-        for (int num : nums2) {
-            if (hm2.containsKey(num)) {
-                int currVal2 = hm2.get(num);
-                hm2.put(num, currVal2++);
-            }
-            else {
-                hm2.put(num, 1);
-            }
-        }
+        // Iterates through levels
+        for (int k = 0; k < a; k++) {
+            int val;
+            int last = 0;
 
-        for (int key : hm1.keySet()) {
-            if (hm2.containsKey(key)) {
-                int v1 = hm1.get(key);
-                int v2 = hm2.get(key);
-                int max = Math.max(v1, v2);
+            // Iterates through each element within a level
+            for (int i = 0; i < k + 1; i++) {
+                val = res.get(i);
 
-                for (int i = 0; i < max; i++) {
-                    arrList.add(key);
+                // Condition states we are reading from within a valid
+                // range.
+                if (i - 1 >= 0) {
+
+                    val += last;
                 }
+                last = res.get(i);
+                res.set(i, val);
             }
+
+            res.add(res.get(0)); // Adds the ender 1
         }
 
-        int[] returnArr = new int[arrList.size()];
-        for (int i = 0; i < arrList.size(); i++) {
-            returnArr[i] = arrList.get(i);
-        }
-        return returnArr;
-    }
-    public static int minPatches(int[] nums, int n) {
-        long miss = 1, added = 0;
-        int i = 0;
-        while (miss <= n) {
-            if (i < nums.length && nums[i] <= miss) {
-                miss += nums[i++];
-            }
-            else {
-                miss += miss;
-                added++;
-            }
-        }
-        return (int)added;
+        return res;
     }
 
     public static String reverseString(String s) {
