91f29de000bbdca3820538210fd8ab69944ba2d0
kevinto <to.thanh.kevin@gmail.com>
2016-06-03 09:47:30 -0700

    Added new explore problem

--- Misc/Explore.java
+++ Misc/Explore.java
@@ -1,15 +1,39 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Stack;
+import java.util.Comparator;
+import java.util.*;
+
 
 /**
  * Created by kevint on 4/27/2016.
  */
 public class Explore {
     public static void main(String[] args) {
-        ArrayList<Integer> test = getRow(2);
-        System.out.println((test));
+        for (int i = 0; i < 100; i += 2) {
+            System.out.println(i);
+        }
+//        ArrayList<Integer> test = getRow(2);
+//        System.out.println((test));
+    }
+
+    public static String[] answer(String[] names) {
+        // Awesome, i can use arrays.sort. need to write comparator now.
+        // 1. do mergesort. compare values on merge step.
+        // figuring out the values on every merge space is troublesome
+
+        // 2. create hash map with key as string and value as value.
+        // do merge sort, but use map as guide for value compare.
+
+
+        // Your code goes here.
+        Arrays.sort(names);
+        return names;
+    }
+
+    class nameComparator implements Comparator<String>{
+        @Override
+        public int compare(String s1, String s2) {
+            return -1;
+        }
     }
 
     public static ArrayList<Integer> getRow(int a) {
