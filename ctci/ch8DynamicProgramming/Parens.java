062ecdd42422a351b27c0c27d2f2ab46353c4c34
kevinto <to.thanh.kevin@gmail.com>
2016-06-19 14:59:35 -0700

    Added a different implementation of get parenthesis

--- ctci/ch8DynamicProgramming/Parens.java
+++ ctci/ch8DynamicProgramming/Parens.java
@@ -10,13 +10,16 @@ import java.util.Iterator;
 public class Parens {
     public static void main(String args[]) {
         int n = 3;
-        System.out.println(getParens(n));
+        System.out.println("getparens: " + getParens(n));
+        System.out.println("GenParensNoDup: " + GenParensNoDup(n));
 
         n = 2;
-        System.out.println(getParens(n));
+        System.out.println("getparens" + getParens(n));
+        System.out.println("GenParensNoDup: " + GenParensNoDup(n));
 
         n = 1;
-        System.out.println(getParens(n));
+        System.out.println("getparens" + getParens(n));
+        System.out.println("GenParensNoDup: " + GenParensNoDup(n));
     }
 
     public static ArrayList<String> getParens(int n) {
@@ -42,5 +45,33 @@ public class Parens {
         }
 
         return result;
+    }
+
+    public static ArrayList<String> GenParensNoDup(int count) {
+        char[] str = new char[count * 2];
+        ArrayList<String> list = new ArrayList<>();
+        addParenNoDup(list, count, count, str, 0);
+        return list;
+    }
+
+    public static void addParenNoDup(ArrayList<String> list, int leftRem, int rightRem,
+                              char[] str, int count) {
+        if (leftRem < 0 || rightRem < leftRem) return;
+
+        if (leftRem == 0 && rightRem == 0) {
+            String s = String.copyValueOf(str);
+            list.add(s);
+            return;
+        } else {
+            if (leftRem > 0) {
+                str[count] = '(';
+                addParenNoDup(list, leftRem - 1, rightRem, str, count + 1);
+            }
+
+            if (rightRem > 0) {
+                str[count] = ')';
+                addParenNoDup(list, leftRem, rightRem - 1, str, count + 1);
+            }
+        }
     }
 }
