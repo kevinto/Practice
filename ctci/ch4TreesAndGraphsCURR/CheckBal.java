c95012277deb40d6c2c57fe2cb6de559c4449182
kevinto <to.thanh.kevin@gmail.com>
2016-06-26 15:43:57 -0700

    Added the check balance problem

---
+++ ctci/ch4TreesAndGraphsCURR/CheckBal.java
@@ -0,0 +1,50 @@
+package ch4TreesAndGraphsCURR;
+
+/**
+ * Created by Kevin on 6/26/16.
+ */
+public class CheckBal {
+    public static void main(String[] args) {
+        BalNode root = new BalNode(5);
+        root.left = new BalNode(6);
+//        root.left.left = new BalNode(7);
+//        root.left.left.left = new BalNode(8);
+//        root.right = new BalNode(9);
+
+        System.out.println(balanced(root));
+    }
+
+    private static boolean balanced(BalNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        int leftHeight = balHelper(root.left) + 1;
+        int rightHeight = balHelper(root.right) + 1;
+        return Math.abs(leftHeight - rightHeight) <= 1;
+    }
+
+    private static int balHelper(BalNode root) {
+        if (root == null ) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 0;
+        }
+
+        int leftHeight = balHelper(root.left) + 1;
+        int rightHeight = balHelper(root.right) + 1;
+        return Math.max(leftHeight, rightHeight);
+    }
+}
+
+class BalNode {
+    int val;
+    BalNode left;
+    BalNode right;
+
+    BalNode(int x) {
+        this.val = x;
+    }
+}
