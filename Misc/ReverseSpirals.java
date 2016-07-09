00e2b844b724514af81faabb1e3cf15b5512a306
kevinto <to.thanh.kevin@gmail.com>
2016-07-08 18:20:42 -0700

    Added solution to reverse spirals problem

---
+++ Misc/ReverseSpirals.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kevin on 7/3/16.
+ * Spiral Array Problem
+ */
+public class ReverseSpirals {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+
+        List<Integer> res = new ReverseSpirals().spiralOrder(matrix);
+        System.out.println(res);
+    }
+
+    private List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) return res;
+
+        int currTopRow = 0;
+        int currBotRow = matrix.length - 1;
+        int currLeftCol = 0;
+        int currRightCol = matrix[0].length - 1;
+
+        while (currTopRow <= currBotRow && currLeftCol <= currRightCol) {
+            // Save bottom row
+            for (int i = currRightCol; i >= currLeftCol; i--) {
+                res.add(matrix[currBotRow][i]);
+            }
+
+            // Save left col
+            for (int i = currBotRow - 1; i >= currTopRow; i--) {
+                res.add(matrix[i][currLeftCol]);
+            }
+
+            // Save top row
+            for (int i = currLeftCol + 1; i <= currRightCol; i++) {
+                res.add(matrix[currTopRow][i]);
+            }
+
+            // Save right col
+            for (int i = currTopRow + 1; i <= currBotRow - 1; i++) {
+                res.add(matrix[i][currRightCol]);
+            }
+            currTopRow++;
+            currBotRow--;
+            currLeftCol++;
+            currRightCol--;
+        }
+
+        return res;
+    }
+}
