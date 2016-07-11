e0cccd1275dd588cb311810484d56770b046d0e4
kevinto <to.thanh.kevin@gmail.com>
2016-07-10 19:06:50 -0700

    Added more solutions to check per problem

--- ctci/ch1ArraysAndStrings/CheckPerm.java
+++ ctci/ch1ArraysAndStrings/CheckPerm.java
@@ -1,4 +1,8 @@
 package ch1ArraysAndStrings;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
 /*
  * Problem 1.1
  * Problem 1.1 Statement:
@@ -10,52 +14,102 @@ public class CheckPerm {
         runTests();
     }
     
-    public static boolean isPerm(String s1, String s2) {
+    public static boolean isPermUsingArrayMap(String s1, String s2) {
         if (s1.length() != s2.length()) {
             return false;
         }
         
-        int[] map = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            char curChar = s1.charAt(i);
-            int curInt = (int) curChar % 97;
-            
-            if (curInt < 0 || 25 < curInt) {
-                return false;
-            }
-            else {
-                map[curInt] += 1;
-            }
+        int[] letters = new int[128];
+
+        char[] s1_array = s1.toCharArray();
+        for (char c : s1_array) {
+            letters[c]++;
         }
-        
+
+        // This way works because if a letter in s2 doesnt exist in s1, then
+        // the letter map will be 0 for that location and will go negative.
+        // if we have the same letter counts for both, and we have more of a
+        // certain letter than another, then will will have to go negative somewhere.
         for (int i = 0; i < s2.length(); i++) {
-            char curChar = (char) s2.charAt(i);
-            int curInt = ((int) curChar) % 97;
-            
-            if (curInt < 0 || 25 < curInt) {
-                return false;
-            }
-            else {
-                map[curInt] -= 1;
-            }
-        }
-        
-        for (int i = 0; i < map.length; i++) {
-            if (map[i] != 0) {
+            int c = (int)s2.charAt(i);
+            letters[c]--;
+            if (letters[c] < 0) {
                 return false;
             }
         }
-        
+
         return true;
     }
-    
+
+    public static boolean isPermUsingSort(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        return sort(s1).equals(sort(s2));
+    }
+
+    public static String sort(String str) {
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
+    }
+
+    public static boolean isPermUsingHashMap(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            char currChar = s1.charAt(i);
+            if (!map.containsKey(currChar)) {
+                map.put(currChar, 1);
+            } else {
+                map.put(currChar, map.get(currChar) + 1);
+            }
+        }
+
+        for (int i = 0; i < s2.length(); i++) {
+            char currChar = s2.charAt(i);
+            if (!map.containsKey(currChar)) {
+                return false;
+            } else {
+                int currVal = map.get(currChar);
+                if (currVal - 1 < 0) {
+                    return false;
+                }
+                map.put(currChar, currVal - 1);
+            }
+        }
+
+        for (char key : map.keySet()) {
+            if (map.get(key) != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void runTests() {
         boolean result = true;
         
-        result &= isPerm("abc", "bac");
-        result &= !isPerm("abc", "bace");
-        result &= !isPerm("abc", "bae");
-        
+//        result &= isPermUsingArrayMap("abc", "bac");
+//        result &= !isPermUsingArrayMap("abc", "bace");
+//        result &= !isPermUsingArrayMap("abc", "bae");
+//        result &= !isPermUsingArrayMap("aab", "bae");
+
+//        result &= isPermUsingSort("abc", "bac");
+//        result &= !isPermUsingSort("abc", "bace");
+//        result &= !isPermUsingSort("abc", "bae");
+//        result &= !isPermUsingSort("aab", "bae");
+
+        result &= isPermUsingHashMap("abc", "bac");
+        result &= !isPermUsingHashMap("abc", "bace");
+        result &= !isPermUsingHashMap("abc", "bae");
+        result &= !isPermUsingHashMap("aab", "bae");
+
         if (result) {
             System.out.println("All tests passed!");
         }
