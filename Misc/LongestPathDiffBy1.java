3c6e3186ae5768413de36a89f91ca97d5b580724
kevinto <to.thanh.kevin@gmail.com>
2016-07-15 09:08:17 -0700

    Partial solution to longest path

--- Misc/LongestPathDiffBy1.java
+++ Misc/LongestPathDiffBy1.java
@@ -4,29 +4,48 @@ import java.util.ArrayList;
  * Created by Kevin on 7/14/16.
  */
 public class LongestPathDiffBy1 {
+    public static void main(String[] args) {
+        LongDiffNode root = new LongDiffNode(9);
+        root.left = new LongDiffNode(8);
+        root.right = new LongDiffNode(10);
+
+        root.left.left = new LongDiffNode(6);
+        root.left.left.left = new LongDiffNode(5);
+        root.left.left.left.left = new LongDiffNode(4);
+
+        findLongest(root);
+        return;
+    }
+
     public static String findLongest(LongDiffNode root) {
         if (root == null) return "";
 
-        ArrayList<String> paths = new ArrayList<>();
-        findLongest(root, "", paths, 0);
-
+        ArrayList<ArrayList<Integer>> paths = new ArrayList<>();
+        findLongest(root, new ArrayList<>(), paths, Integer.MAX_VALUE);
+        // TODO: we can use a heap here to keep track if the longest path.
         return "";
     }
 
-    public static void findLongest(LongDiffNode root, String path, ArrayList<String> paths, int prevVal) {
-        boolean diffby1 = Math.abs(prevVal - root.val) != 1;
-        if (!diffby1) {
-            if (!path.isEmpty()) paths.add(path);
-            path = "";
+    public static void findLongest(LongDiffNode root, ArrayList<Integer> path, ArrayList<ArrayList<Integer>> paths, int prevVal) {
+        if (root == null) return;
+        boolean diffby1 = Math.abs(prevVal - root.val) == 1;
+        if (!diffby1 && !path.isEmpty()) {
+            paths.add(new ArrayList<>(path));
+            path = new ArrayList<>(); // if i convert to arraylist then i lose track of upstream
         }
-        path += root.val;
+        path.add(root.val);
 
         if (root.left == null && root.right == null) {
-            if (diffby1 && !path.isEmpty()) {
-                paths.add(path);
+            if (diffby1 && path.size() > 1) {
+                paths.add(new ArrayList<>(path));
             }
+            path.remove(path.size() - 1);
             return;
         }
+
+        findLongest(root.left, path, paths, root.val);
+        findLongest(root.right, path, paths, root.val);
+        path.remove(path.size() - 1);
     }
 }
 
@@ -34,4 +53,7 @@ class LongDiffNode {
     LongDiffNode left;
     LongDiffNode right;
     int val;
+    LongDiffNode (int v) {
+        this.val = v;
+    }
 }
