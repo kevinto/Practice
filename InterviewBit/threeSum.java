941fd577848acc334d1d7e3ea71a7f29c3785a7d
kevinto <to.thanh.kevin@gmail.com>
2016-07-18 08:59:57 -0700

    Added solution to 4sum

---
+++ InterviewBit/threeSum.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Kevin on 7/17/16.
+ */
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] test1 = { -1, 0, 1, 2, -1, -4 };
+        System.out.println(threeSum(test1));
+    }
+
+    public static List<List<Integer>> threeSum(int[] num) {
+        Arrays.sort(num);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < num.length-2; i++) {
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                        while (lo < hi && num[lo] == num[lo+1]) lo++;
+                        while (lo < hi && num[hi] == num[hi-1]) hi--;
+                        lo++; hi--;
+                    } else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
+                }
+            }
+        }
+        return res;
+    }
+}
