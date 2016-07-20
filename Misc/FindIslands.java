ac792bc07899fe0706a40ddb9ec616b97b75b3d5
kevinto <to.thanh.kevin@gmail.com>
2016-07-19 21:40:06 -0700

    Added find islands problem

--- Misc/FindIslands.java
+++ Misc/FindIslands.java
@@ -9,7 +9,52 @@ public class FindIslands {
                 {1, 1, 1 ,0},
                 {0, 0, 0 ,1}
         };
-        System.out.print(numIslands(grid, grid.length, grid[0].length));
+        System.out.println("2d approach: " + numIslands(grid, grid.length, grid[0].length));
+
+        int[] grid2 = {
+                1, 0, 0, 1,
+                0, 0, 0 ,1,
+                1, 1, 1 ,0,
+                0, 0, 0 ,1
+        };
+        System.out.println("1d approach: " + numIslands(grid2, 4, 4));
+    }
+
+    public static int numIslands(int[] grid, int rowCount, int colCount) {
+        if (grid == null) return 0;
+
+        int islandCount = 0;
+        for (int row = 0; row < rowCount; row++) {
+            for (int col = 0; col < colCount; col++) {
+                int index = (colCount * row) + col;
+                if (grid[index] == 1) {
+                    islandCount++;
+                    turn1To0(grid, row, col, rowCount, colCount);
+                }
+            }
+        }
+        return islandCount;
+    }
+
+    public static void turn1To0(int[] grid, int row, int col, int totalRow, int totalCol) {
+        int index = (totalCol * row) + col;
+        if (row >= totalRow || col >= totalCol || row < 0 || col < 0) return;
+        if (index < 0 || index >= grid.length) return;
+        if (grid[index] == 0) return;
+
+        grid[index] = 0;
+
+        // Check top
+        turn1To0(grid, row - 1, col,totalRow, totalCol);
+
+        // Check bot
+        turn1To0(grid, row + 1, col,totalRow, totalCol);
+
+        // Check left
+        turn1To0(grid, row, col - 1,totalRow, totalCol);
+
+        // Check right
+        turn1To0(grid, row, col + 1,totalRow, totalCol);
     }
 
     public static int numIslands(int[][] grid, int rowCount, int colCount) {
@@ -18,6 +63,8 @@ public class FindIslands {
         int islandCount = 0;
         for (int row = 0; row < rowCount; row++) {
             for (int col = 0; col < colCount; col++) {
+                int index = (colCount * row) + col;
+
                 if (grid[row][col] == 1) {
                     islandCount++;
                     turn1To0(grid, row, col, rowCount, colCount);
