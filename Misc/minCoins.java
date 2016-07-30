69b72e4924157a3e1481508cb460e6212b53b609
kevinto <to.thanh.kevin@gmail.com>
2016-07-30 15:49:22 -0700

    Added space efficient solution to min coin problem

--- Misc/minCoins.java
+++ Misc/minCoins.java
@@ -1,14 +1,68 @@
 /**
  * Created by Kevin on 7/30/16.
+ *
  */
 public class minCoins {
     public static void main(String[] args) {
-        int[] denoms1 = {1, 2, 3};
-        int total1 = 5;
-        System.out.println(minCoins(denoms1, total1));
+//        int[] denoms1 = {1, 2, 3};
+//        int total1 = 5;
+//        minCoinsWith2dMemo(denoms1, total1);
+//
+//        int[] denoms2 = {1, 5, 6, 8};
+//        int total2 = 11;
+//        minCoinsWith2dMemo(denoms2, total2);
+
+        int[] denoms3 = {8, 5, 6, 1};
+        int total3 = 17;
+        minCoinUsingLinearSpace(denoms3, total3);
     }
 
-    public static int minCoins(int[] denoms, int total) {
+    public static void minCoinUsingLinearSpace(int[] denoms, int total) {
+        int[] minCt = new int[total + 1];
+        int[] coinUsed = new int[total + 1];
+
+        initMinCt(minCt);
+        initCoinUsed(coinUsed);
+
+        for (int coinIdx = 0; coinIdx < denoms.length; coinIdx++) {
+            for (int minCtIdx = denoms[coinIdx]; minCtIdx < minCt.length; minCtIdx++) {
+                if (minCt[minCtIdx - denoms[coinIdx]] + 1 < minCt[minCtIdx]) {
+                    minCt[minCtIdx] = minCt[minCtIdx - denoms[coinIdx]] + 1;
+                    coinUsed[minCtIdx] = coinIdx;
+                }
+            }
+        }
+        findPathUsingLinearSpace(coinUsed, denoms);
+        System.out.print("linear space min coin for " + total + ": " + minCt[minCt.length - 1]);
+    }
+
+    private static void findPathUsingLinearSpace(int[] coinUsed, int[] denoms) {
+        int i = coinUsed.length - 1;
+
+        System.out.print("Path: ");
+        while (i > 0) {
+            int coinValue = denoms[coinUsed[i]];
+            System.out.print(coinValue + " ");
+            i -= coinValue;
+        }
+        System.out.println();
+    }
+
+    private static void initMinCt(int[] minCt) {
+        // Start at 1 because a total of 0 will make 0 coins to make up
+        for (int i = 1; i < minCt.length; i++) {
+            minCt[i] = Integer.MAX_VALUE - 1;
+        }
+    }
+
+    private static void initCoinUsed(int[] coinUsed) {
+        // Using 1 to indicate no optimal coin found for this total.
+        for (int i = 0; i < coinUsed.length; i++) {
+            coinUsed[i] = -1;
+        }
+    }
+
+    public static int minCoinsWith2dMemo(int[] denoms, int total) {
         if (denoms == null || denoms.length == 0) {
             return -1;
         }
@@ -36,8 +90,37 @@ public class minCoins {
             }
         }
 
-        printMatrix(memo);
+        printPathFor2dMemo(memo, denoms);
+        System.out.println("min coins for " + total + ": " + memo[memo.length - 1][memo[0].length - 1]);
         return memo[memo.length - 1][memo[0].length - 1];
+    }
+
+    private static void printPathFor2dMemo(int[][] memo, int[] denom) {
+        int row = memo.length - 1;
+        int col = memo[0].length - 1;
+
+        System.out.print("Printing path: ");
+        while (row >= 0 && col > 0) {
+            if (row == 0) {
+                if (memo[row][col - denom[row]] + 1 != memo[row][col]) {
+                    System.out.println("Error: No path exists");
+                    return;
+                } else {
+                    System.out.print(denom[row] + " ");
+                    col -= denom[row];
+                    continue;
+                }
+            }
+
+            if (memo[row - 1][col] == memo[row][col]) {
+                row -= 1;
+                continue;
+            } else {
+                System.out.print(denom[row] + " ");
+                col -= denom[row];
+            }
+        }
+        System.out.println("");
     }
 
     private static void initMemo(int[][] memo) {
