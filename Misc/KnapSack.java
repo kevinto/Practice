ecd6a596bf8817d119fd005d705cac4238d8f757
kevinto <to.thanh.kevin@gmail.com>
2016-07-30 18:23:17 -0700

    added a backtracking solution to the knapsack problem

--- Misc/KnapSack.java
+++ Misc/KnapSack.java
@@ -29,8 +29,29 @@ public class KnapSack {
         int[] val2 = { 60, 100, 120 };
         int[] weights2 = { 10, 20, 30 };
 
-        int result = new KnapSack(numItems, val2, weights2, capacity).findMaxVal();
-        out.println("Max value we can have in the knapsack: " + result);
+//        int result = new KnapSack(numItems, val2, weights2, capacity).findMaxVal();
+//        out.println("Max value we can have in the knapsack: " + result);
+        int optimal2 = knapSackBacktrack(capacity, weights2, val2, val2.length);
+        out.println("Max value using backtracking: " + optimal2);
+    }
+
+    private static int knapSackBacktrack(int W, int wt[], int val[], int n)
+    {
+        // Base Case
+        if (n == 0 || W == 0)
+            return 0;
+
+        // If weight of the nth item is more than Knapsack capacity W, then
+        // this item cannot be included in the optimal solution
+        if (wt[n-1] > W)
+            return knapSackBacktrack(W, wt, val, n-1);
+
+            // Return the maximum of two cases:
+            // (1) nth item included
+            // (2) not included
+        else return Math.max(val[n-1] + knapSackBacktrack(W-wt[n-1], wt, val, n-1),
+                knapSackBacktrack(W, wt, val, n-1)
+        );
     }
 
     private KnapSack(int numItems, int[] val, int[] weights, int capacity) {
