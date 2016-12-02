f5957ae4e2657f0c0309dca541693c7b57b1c05a
kevinto <to.thanh.kevin@gmail.com>
2016-12-02 09:48:19 -0800

    Updated the exp eval with the correct evaluator code

--- IK/ExpressionEvaluator.java
+++ IK/ExpressionEvaluator.java
@@ -2,6 +2,11 @@
  * Created by kevinto on 12/1/16.
  */
 public class ExpressionEvaluator {
+    public static void main(String[] args) {
+        char[] operators = {'c', '+', '*'};
+        System.out.println(evaluateExpression("2222", operators));
+    }
+
     public String[] expCreator(String strDigits, int k) {
         char[] operators = new char[strDigits.length() - 1];
         expCreator(strDigits, k, operators, 0);
@@ -10,7 +15,7 @@ public class ExpressionEvaluator {
 
     private void expCreator(String operands, int target, char[] operators, int writeIndex) {
         if (writeIndex >= operands.length() - 1) {
-            if (evaluate(operands, operators) == target) {
+            if (evaluateExpression(operands, operators) == target) {
                 System.out.print("Found solution");
             }
             return;
@@ -24,35 +29,46 @@ public class ExpressionEvaluator {
         expCreator(operands, target, operators, writeIndex + 1);
     }
 
-    private int evaluate(String operands, char[] operators) {
+    private static int evaluateExpression(String digits, char[] operators) {
         int result = 0;
-        boolean multiply = false;
+        boolean multiplication = false;
         int factor = 1;
-        int length = operands.length();
 
-        for (int i = 0; ; i++) {
-            int digit = operands.charAt(i) - '0';
-            while (i < length && operators[i] == 'c') {
-                digit = 10 * digit + operands.charAt(++i) - '0';
+        for(int i = 0; ; ++i) {
+            int operand = digits.charAt(i) - '0';
+
+            // Take care of concat first
+            while(i < operators.length && operators[i] == 'c')
+                operand = 10 * operand + digits.charAt(++i) - '0';
+
+            // Second, take care of multiplication.
+            // Why don't we have a continue here?
+            //  This multiplies the curr num with the previous num.
+            //  The pointer can be on a plus. We multiply before
+            //  we deal with the plus.
+            if (multiplication) {
+                operand *= factor;
+                multiplication = false;
             }
 
-            if (multiply) {
-                digit *= factor;
-                multiply = false;
-            }
-
-            if (i == length - 1) {
-                result += digit;
+            // We reached the end, exit the loop.
+            if (i == operators.length) {
+                result += operand;
                 break;
             }
 
+            // Third, take care of adds. When we reach an add,
+            // we know to add all the stuff on the left side.
             if (operators[i] == '+') {
-                result += operands.charAt(i) - '0';
+                result += operand;
                 continue;
             }
 
-            multiply = true;
-            factor = digit;
+            // Why is this here?
+            //  Need to save the left as a factor, so I can
+            //  multiply it by the right.
+            multiplication = true;
+            factor = operand;
         }
 
         return result;
