54fff663c8e8ed157820289e706cf2cbc93ca1c7
kevinto <to.thanh.kevin@gmail.com>
2017-01-14 17:09:44 -0800

    Added the sum zero problem

---
+++ IK/SumZero.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by kevinto on 1/14/17.
+ */
+public class SumZero {
+    public static void main(String[] args) {
+        int[] nums = {5, 1, 2,-3 ,7 ,-4};
+        String[] res = sumZero(nums);
+
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
+
+        sumZeroBrute(nums);
+    }
+
+    static void sumZeroBrute(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        for (int start = 0; start < nums.length; start++) {
+            int sum = 0;
+            for (int end = start; end < nums.length; end++) {
+                sum += nums[end];
+
+                if (sum == 0) {
+                    System.out.println("Found sum 0 at start: " + start + ", end: " + end);
+                }
+            }
+        }
+    }
+
+    static String[] sumZero(int[] nums) {
+        List<String> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return strListToStrArr(res);
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (map.containsKey(sum)) {
+                res.add(getSubArray(nums, map.get(sum) + 1, i));
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return strListToStrArr(res);
+    }
+
+    static String getSubArray(int[] nums, int start, int end) {
+        if (start > end) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(nums[start]);
+        for (int i = start + 1; i <= end; i++) {
+            sb.append("," + Integer.toString(nums[i]));
+        }
+
+        return sb.toString();
+    }
+
+    static String[] strListToStrArr(List<String> res) {
+        String[] resArr = new String[res.size()];
+
+        for (int i = 0; i < res.size(); i++) {
+            resArr[i] = res.get(i);
+        }
+
+        return resArr;
+    }
+}
--- Misc/Solution.java
+++ Misc/Solution.java
@@ -3,65 +3,37 @@ import java.util.HashSet;
 
 public class Solution {
     public static void main(String[] args) {
-        Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
-
-        alternativeSplit(head);
-    }
-
-    public static void alternativeSplit(Node head) {
-        if (head == null) {
-            return;
-        }
-
-        Node head1 = head;
-        Node tail1 = head;
-        Node head2 = null;
-        Node tail2 = null;
-        Node curr = head;
-        int count = 1;
-
-        while (curr != null) {
-            if (count % 2 == 1) {
-                tail1.next = curr;
-                tail1 = curr;
-            } else {
-                if (head2 == null) {
-                    head2 = curr;
-                    tail2 = curr;
-                } else {
-                    tail2.next = curr;
-                    tail2 = curr;
-                }
-            }
-
-            curr = curr.next;
-            count++;
-        }
-
-        tail1.next = null;
-        tail2.next = null;
-
-        printLL(head1);
-        printLL(head2);
-    }
-
-    public static void printLL(Node head) {
-        Node curr = head;
-        while (curr != null) {
-            System.out.println(curr.val);
-            curr = curr.next;
+        HashSet<Tile> visited = new HashSet<>();
+        visited.add(new Tile(1, 1));
+        if (visited.contains(new Tile(1, 1))) {
+            System.out.println("passed");
+        } else {
+            System.out.println("failed");
         }
     }
 
-    static class Node {
-        Node next;
-        int val;
+    private static class Tile {
+        int row;
+        int col;
+        int moves;
 
-        Node(int newVal) {
-            this.val = newVal;
+        Tile(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        Tile(int row, int col, int moves) {
+            this.row = row;
+            this.col = col;
+            this.moves = moves;
+        }
+
+        public boolean equals(Tile t2) {
+            return this.row == t2.row && this.col == t2.col;
+        }
+
+        public int hashCode() {
+            return 0;
         }
     }
 }
