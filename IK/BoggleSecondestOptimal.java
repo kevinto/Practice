42213bf2946e26a6d0ceb1dc2eec6968e1fe93bc
kevinto <to.thanh.kevin@gmail.com>
2017-01-19 18:46:28 -0800

    Added second most optimal boggle solution file

---
+++ IK/BoggleSecondestOptimal.java
@@ -0,0 +1,10 @@
+/**
+ * Created by kevinto on 1/19/17.
+ While the queue is not empty:
+     Dequeue a triple (x, y, s)
+     For each square (x', y') with letter c adjacent to (x, y):
+         If s+c is a word, output s+c
+         If s+c is a prefix of a word, insert (x', y', s+c) into the queue
+ */
+public class BoggleSecondestOptimal {
+}
