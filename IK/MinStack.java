49ed5e85c9a5ac7b194f3e19ce2491bf9fd21349
kevinto <to.thanh.kevin@gmail.com>
2017-01-22 15:53:33 -0800

    Added minstack testing method

--- IK/MinStack.java
+++ IK/MinStack.java
@@ -4,6 +4,21 @@ import java.util.Stack;
  * Created by kevinto on 12/12/16.
  */
 public class MinStack {
+    public static void main(String[] args) {
+        MinStack stack = new MinStack();
+
+        stack.push(5);
+        stack.push(4);
+        stack.push(4);
+        stack.push(2);
+        stack.push(6);
+
+        System.out.println("min: " + stack.getMin());
+        stack.pop();
+        stack.pop();
+        System.out.println("min: " + stack.getMin());
+    }
+
     private Stack<Integer> stack;
     private Stack<Integer> minStack;
 
@@ -16,9 +31,7 @@ public class MinStack {
     public void push(int x) {
         stack.push(x);
 
-        if (minStack.empty()) {
-            minStack.push(x);
-        } else if (minStack.peek() >= x) {
+        if (minStack.empty() || minStack.peek() >= x) {
             minStack.push(x);
         }
     }
