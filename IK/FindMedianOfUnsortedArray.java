d433f712ca913793620e41bca66621af4206eb2f
kevinto <to.thanh.kevin@gmail.com>
2017-02-01 19:02:25 -0800

    Added find median implementation

--- IK/FindMedianOfUnsortedArray.java
+++ IK/FindMedianOfUnsortedArray.java
@@ -1,9 +1,77 @@
+import java.util.PriorityQueue;
+
 /**
  * Created by kevinto on 1/10/17.
  *
  * Useful Link: http://cs.stackexchange.com/questions/1914/to-find-the-median-of-an-unsorted-array
  */
 public class FindMedianOfUnsortedArray {
+    public static int findMedQs(int[] nums) {
+        int half = nums.length / 2;
+        int medPos = nums.length % 2 == 0 ? half - 1 : half;
+        int start = 0;
+        int end = nums.length - 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            int pivot = nums[start];
+            int pivotIdx = start;
+            int oldStart = start;
+            int oldEnd = end;
+
+            while (start <= end) {
+                while (start <= end && nums[start] <= pivot) {
+                    start++;
+                }
+
+                while (start <= end && nums[end] > pivot) {
+                    end--;
+                }
+
+                if (start <= end) {
+                    swap(nums, start, end);
+                    start++;
+                    end--;
+                }
+            }
+
+            swap(nums, end, pivotIdx);
+
+            if (end == medPos) {
+                break;
+            } else if (end < medPos) {
+                start = end + 1;
+                end = oldEnd;
+            } else if (end > medPos) {
+                start = oldStart;
+                end = end - 1;
+            }
+        }
+
+        return nums[end];
+    }
+
+    private static void swap(int[] nums, int i1, int i2) {
+        int temp = nums[i1];
+        nums[i1] = nums[i2];
+        nums[i2] = temp;
+    }
+
+    public static int findMedianHeaps(int[] nums) {
+        // TODO: input checking
+
+        int heapSize = (nums.length / 2) + 1;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for (int val : nums) {
+            queue.offer(val);
+        }
+
+        while (queue.size() > heapSize) {
+            queue.poll();
+        }
+
+        return queue.poll();
+    }
 }
 
 /*
